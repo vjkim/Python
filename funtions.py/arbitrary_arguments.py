d523110ce501aedc18b32d1d861d80e4b258c709
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:31:16 -0400

    Arbitrary Arguments

---
+++ funtions.py/arbitrary_arguments.py
@@ -0,0 +1,8 @@
+### 8/20/2020
+### Arbitrary Arguments
+
+def create_passenger(*requests):
+	""" Print user requests"""
+	print(requests)
+
+create_passenger('window seat', 'seat near the top of the plane', 'preorder breakfast')
