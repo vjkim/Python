4a694d4e1db8e01d8849e25a13542a7e6b9ee11f
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:26:00 -0400

    Using Continue in a loop

---
+++ input.py/continue_loop.py
@@ -0,0 +1,12 @@
+### 8/18/2020
+### Using Continue in a loop
+
+
+current_number = 0
+while current_number < 10:
+	current_number += 1
+	if current_number % 2 == 0:
+		Continue
+
+
+print(current_number)
