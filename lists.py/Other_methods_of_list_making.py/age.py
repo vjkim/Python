0c840f218bd560e434f67ac5937228d0ed66d6fd
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:23:01 -0400

    age

---
+++ lists.py/Other_methods_of_list_making.py/age.py
@@ -0,0 +1,18 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age = 18
+>>> age == 18
+True
+>>> 
+= RESTART: C:/Users/vjkny/OneDrive/Desktop/Python/lists.py/Other_methods_of_list_making.py/numbers.py
+Sorry, no cake for you.
+>>> age = 21
+>>> age < 21
+False
+>>> age > 21
+False
+>>> age <= 21
+True
+>>> age >= 21
+True
+>>> 
