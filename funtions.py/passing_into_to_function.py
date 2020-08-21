57c6e8f4d502ca5c4fe0e9be1225059e19f96376
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:27:57 -0400

    Passing Information to a function

---
+++ funtions.py/passing_into_to_function.py
@@ -0,0 +1,11 @@
+### 8/18/2020
+### Passing Information to a function
+
+def hello_world(username):
+#username = parameter
+	""""Showing a username"""
+	print("Hello " + username.title() + ".")
+
+hello_world('tony')
+
+# multiple parameters = multiple things
