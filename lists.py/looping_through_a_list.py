53422207b1fd6fa09dac7e46c27344d9081bf325
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:24:20 -0400

    looping thru a list

---
+++ lists.py/looping_through_a_list.py
@@ -0,0 +1,15 @@
+### 8/10/2020
+### V
+### Looping through a list
+
+months = ['jan','feb', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+
+# Using a For loop to print a list
+for month in months: 
+	print(month.title() + "\n") 
+	print("the next month is: ")
+
+print("Goodbye")
+
+
+
