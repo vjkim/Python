590ebf145c375e7a3aca37bea3a8ebcbe646d030
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:28:33 -0400

    Making arguements optional

---
+++ funtions.py/optional_requirements.py
@@ -0,0 +1,24 @@
+### 8/19/2020
+### Making arguements optional
+
+def formatted_name(first_name, last_name,  middle_name):
+	"""Return a full name"""
+	full_name = first_name + ' ' + middle_name + ' '+ last_name
+	return full_name.title()
+
+teacher = formatted_name('tony', 'james', 'staunton')
+
+def formatted_name(first_name, middle_name, last_name):
+	"""Return a full name"""
+	if middle_name:
+		full_name = first_name + ' ' + middle_name + ' ' + last_name + ' '
+	else:
+		full_name = first_name + ' ' + last_name + ' '
+	return full_name.title()
+
+teacher = formatted_name('tony', 'staunton', 'james')
+
+print(teacher)
+
+teacher = formatted_name('tony','', 'staunton')
+print(teacher)
