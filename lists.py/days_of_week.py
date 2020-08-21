2e54daedae1b7d0a1e4691661b214cfb5de41a57
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:24:44 -0400

    How to find the length of a list

---
+++ lists.py/days_of_week.py
@@ -0,0 +1,8 @@
+### 8/10/2020
+### Author = V
+### How to find the length of a list
+
+#list of days of a week
+birthday_days = ['monday', 'tuesday', 'wednesday','thursday','friday', 'saturday', 'sunday']
+
+print(len(birthday_days))
