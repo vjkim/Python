67e846456d16ade61775b33a58e959c7d78c20d7
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:21:51 -0400

    if statements

---
+++ lists.py/Other_methods_of_list_making.py/if_statement.py
@@ -0,0 +1,12 @@
+### 8/10/2020
+### Python's IF statement
+
+print("To continue, please log in.")
+
+#using the if statement
+password = input("Please enter your password.")
+if password == "tony":
+    print("Access granted.")
+else:
+    print("Incorrect, please try again.")
+
