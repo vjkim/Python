a3db497c544326bf1223a6fcb2d6f8e85f6f733f
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:20:51 -0400

    input statements

---
+++ lists.py/Other_methods_of_list_making.py/input_statements.py
@@ -0,0 +1,16 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> ###8/10/2020
+>>> ### Using the input statement
+>>> 
+>>> welcome = input("Welcome, what's your name? ")
+Welcome, what's your name? 
+>>> 
+>>> welcome = input("Welcome, what's your name? ")
+Welcome, what's your name? 
+>>> print("Hello" + welcome)
+Hello
+>>> 
+============================== RESTART: Shell ==============================
+>>> 
