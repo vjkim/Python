9350d902684854fd9e3a183002052386e4269619
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:26:45 -0400

    REturing a value in a function

---
+++ funtions.py/returning_a_value_in_fnct.py
@@ -0,0 +1,17 @@
+### 8/19/2020
+### REturing a value in a function
+
+def formatted_name(first_name, last_name):
+	"""Return a formatted name"""
+	full_name = first_name + ' ' + last_name
+	return full_name.title()
+
+teacher = formatted_name('v','kim')
+print(teacher)
+
+def get_formatted_username(email_address):
+	"""Return a formatted username"""
+	username = email_address.strip()
+	return username
+user = get_formatted_username('tony@example.com')
+print(user)
