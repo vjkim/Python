a153932fb1a80a12e76abbbfaafec5fa7649d35e
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:22:40 -0400

    Checking multiple conditions with the and condition

---
+++ lists.py/Other_methods_of_list_making.py/and_conditions.py
@@ -0,0 +1,14 @@
+### 8/11/2020
+### Checking multiple conditions with the and condition
+
+username = input("Welcome, please enter your username.")
+password = input("Please enter your password.")
+
+if username != 'tony' and password != 'password123':
+    print("Access Denied.")
+else:
+    print("Welcome.")
+
+    
+
+    
