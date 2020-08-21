a0b87c7a62661206194930d3b616237f63c5a42f
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:23:15 -0400

    Organizing a list

---
+++ lists.py/organizing_a_list.py
@@ -0,0 +1,26 @@
+### 8/10/2020
+### Author - V
+### Organizing a list
+
+# Creating a list of months
+birthday_months = ['may','november', 'june']
+
+birthday_months.sort()
+print(birthday_months)
+
+#using the reverse sort method
+birthday_months = ['may','november', 'june']
+
+birthday_months.sort(reverse=True)
+print(birthday_months)
+
+
+birthday_months = ['may','november', 'june']
+print(sorted(birthday_months))
+
+print(birthday_months)
+
+#list in reverse order
+birthday_days = ['monday', 'tuesday', 'wednesday','thursday','friday']
+birthday_days.reverse()
+print(birthday_days)
