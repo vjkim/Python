2679f26d513ffdd456252ad677d9157798b71b55
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:23:46 -0400

    Looping thru a slice

---
+++ lists.py/Neumerical_lists.py/looping_thru_a_slicing.py
@@ -0,0 +1,13 @@
+### 8/10/2020
+### Looping thru a slice
+
+
+#Looping thru a slice with a for loop
+names = ['tony','frank','mary','carl']
+print("Here are the names of the last 3 registrations")
+for name in names[:3]:
+	print(name.title())
+
+
+
+
