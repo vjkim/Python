a598ddcc071bede6f43233c12c034459502693e2
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:25:54 -0400

    Using the input prompt

---
+++ input.py/input_prompt.py
@@ -0,0 +1,6 @@
+### 8/13/2020
+### Using the input prompt
+
+prompt = "this is our lexture discussing Python's input prompt and how to keep it tidy. Please enter your favorite lecure so far and why you liked it? "
+
+favorite_lecture = input(prompt)
