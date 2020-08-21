6b69439e221bbe0309bc9b1c3bae781aafb6de29
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:29:52 -0400

    Keyword Arguments

---
+++ funtions.py/Keyword_arguements.py
@@ -0,0 +1,13 @@
+### 8/19/2020
+### Keyword Arguments
+
+
+# Create a function
+def book_description(book_type, author):
+	""" Display book information """
+
+	print("\nThis is a "+ book_type + " book.")
+	print("The author is " + author + ".")
+
+book_description(book_type= 'nonfiction', author='brad stone')
+
