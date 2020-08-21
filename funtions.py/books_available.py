d6070c2c4145d4264c90e6c3b4ee3c374dc9de04
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:37:16 -0400

    yeh

--- funtions.py/books_available.py
+++ funtions.py/books_available.py
@@ -7,5 +7,4 @@ def books_available(books):
 		books_in_stock = "The following title is available to buy: " + book.title() + "."
 		print(books_in_stock)
 
-available = ['elon musk', 'the everything store', 'the growth map']
-books_available(available)
+
---
+++ funtions.py/show_books_available.py
@@ -0,0 +1,6 @@
+### 8/20/2020
+### Importing a module
+
+import books_available
+
+show_books_available.books_available('Elon Musk')
