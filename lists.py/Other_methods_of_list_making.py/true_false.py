95421216fca360ca35ccf94e6286f63018f7a2c7
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:18:01 -0400

    true_false.py

---
+++ lists.py/Other_methods_of_list_making.py/true_false.py
@@ -0,0 +1,11 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> password = 'tony'
+>>> password == 'tony'
+True
+>>> password == 'Tony'
+False
+>>> password1 = 'Frank'
+>>> password1.lower() == 'frank'
+True
+>>> 
