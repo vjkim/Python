e07b02f58f2ba5cdb50a94efb618aeed19670d5d
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:30:43 -0400

    Passing a list to a function

---
+++ funtions.py/books_available.py
@@ -0,0 +1,11 @@
+### 8/19/2020
+### Passing a list to a function
+
+def books_available(books):
+	""" Show a list of books available to buy"""
+	for book in books:
+		books_in_stock = "The following title is available to buy: " + book.title() + "."
+		print(books_in_stock)
+
+available = ['elon musk', 'the everything store', 'the growth map']
+books_available(available)
