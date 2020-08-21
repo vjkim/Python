c8d9715ae099c067ce61a5fc982c1dfccfc8d181
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:17:53 -0400

    tuples.py

---
+++ lists.py/Other_methods_of_list_making.py/tuples.py
@@ -0,0 +1,17 @@
+### 8/10/2020
+###Tuples
+
+dates = (1, 4, 6, 11)
+
+print(dates[0])
+print(dates[2])
+
+coordinates = (1001, 5002)
+print("Original Coordinates: ")
+for coordinate in coordinates:
+	print(coordinate)
+
+coordinates = (2002, 7003)
+print("\nNew coordinates: ")
+for coordinate in coordinates:
+	print(coordinate)
