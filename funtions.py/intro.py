1c9a90d70112c8c5a4f9e6d3dff720a102b41b33
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:30:02 -0400

    How to define a function

---
+++ funtions.py/intro.py
@@ -0,0 +1,12 @@
+### 8/18/2020
+### intro to funtions
+### How to define a function 
+
+
+### What is a function? --> they are named blocks of code that are designed to do one specific job.
+
+def hello_world():
+	"""Displaying hello world"""
+	print("Hello World!")
+
+hello_world()
