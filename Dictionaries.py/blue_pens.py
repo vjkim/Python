82f2acaa9c32a16bb3a71d10f599f08a09a7bde5
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:32:43 -0400

    Using a list within a dictionary

---
+++ Dictionaries.py/blue_pens.py
@@ -0,0 +1,16 @@
+### 8/13/2020
+### Using a list within a dictionary
+
+
+my_ordered_car = {
+	'type' : 'standard four saloon',
+	'extras' : ['alloy wheels', 'climate control', 'leather heated seats']
+}
+
+# Print order summary
+
+print("The car you ordered is a " + my_ordered_car['type'] + "with the following extras:")
+
+for extra in my_ordered_car['extras']:
+	print("\t" + extra)
+
