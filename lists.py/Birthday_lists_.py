9fd22c02cc4f7aaaeae996fb158bd662322f3818
V Kim <vjk.nyc@gmail.com>
2020-08-20 21:24:51 -0400

    How to edit lists

---
+++ lists.py/Birthday_lists_.py
@@ -0,0 +1,40 @@
+### 8 /10 / 2020
+### Author - V
+### How to edit lists
+
+
+#Creating a list of months
+birthday_months = ['spril', 'may', 'november']
+
+#print out our list
+print(birthday_months)
+
+#changing an element in the list = april
+birthday_months[0] = 'april'
+
+print(birthday_months)
+
+#using the append method
+birthday_months.append('june')
+
+print(birthday_months)
+
+#Creating an empty list
+birthday_months = []
+
+print(birthday_months)
+
+birthday_months.append('august')
+
+print(birthday_months)
+
+#using the insert method
+birthday_months.insert(0, 'may')
+
+print(birthday_months)
+
+#Using the delete statement
+del birthday_months[1]
+
+print(birthday_months)
+
