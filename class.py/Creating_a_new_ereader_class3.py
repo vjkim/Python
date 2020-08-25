69fb67616037963afe01dbb2df221d7930112b02
V Kim <vjk.nyc@gmail.com>
2020-08-24 21:30:54 -0400

    ereader

--- class.py/Creating_a_new_ereader_class3.py
+++ class.py/Creating_a_new_ereader_class3.py
@@ -1,11 +1,11 @@
 ### 8/24/2020
-### Creating a new e-reader class
+### Modifying an attribute's value directly
 
 class Ereader():
  	"""A class to represent an ereader"""
 
  	def __init__(self, make, model, backlight, battery, screen_type):
- 		""" Initialize the attributes to describe an ereader"""
+ 		"""Initialize the attributes to describe an ereader."""
  		self.make = make
  		self.model = model
  		self.backlight = backlight
@@ -13,14 +13,14 @@ class Ereader():
  		self.screen_type = screen_type
  		self.library_count = 0
 
-# Function to define the class
+#Function to define the class
  	def get_ereader_name(self):
- 		"""Return a formatted descriptive name for our ereader"""
+ 		"""Return a formatted descriptive name for our ereader."""
  		long_name = self.make + " - " + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
  		return long_name.title()
 
-# read library count
-	def read_library_count(self):
+#read library count
+def read_library_count(self):
 		"""Show the amount of ebooks in our kindle library."""
 		print("You have " + str(self.library_count) + "books in your kindle library.")
 
@@ -29,4 +29,6 @@ my_new_ereader = Ereader("Amazon Kindle", "Paperwhite", "Adjustable Backlight" ,
 print(my_new_ereader.get_ereader_name())
 
 # Read library count
+
+my_new_ereader.library_count = 36
 my_new_ereader.read_library_count()
--- class.py/Creating_a_new_ereader_class4.py
+++ class.py/Creating_a_new_ereader_class4.py
@@ -1,11 +1,11 @@
 ### 8/24/2020
-### Creating a new e-reader class
+### Modifying an attribute's value thru a method
 
 class Ereader():
  	"""A class to represent an ereader"""
 
  	def __init__(self, make, model, backlight, battery, screen_type):
- 		""" Initialize the attributes to describe an ereader"""
+ 		"""Initialize the attributes to describe an ereader."""
  		self.make = make
  		self.model = model
  		self.backlight = backlight
@@ -13,20 +13,26 @@ class Ereader():
  		self.screen_type = screen_type
  		self.library_count = 0
 
-# Function to define the class
+#Function to define the class
  	def get_ereader_name(self):
- 		"""Return a formatted descriptive name for our ereader"""
+ 		"""Return a formatted descriptive name for our ereader."""
  		long_name = self.make + " - " + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
  		return long_name.title()
 
-# read library count
-	def read_library_count(self):
+#read library count
+def read_library_count(self):
 		"""Show the amount of ebooks in our kindle library."""
 		print("You have " + str(self.library_count) + "books in your kindle library.")
+
+
+def update_library_count(self, ebook_count):
+		""" Set the library count.""" 
+		self.library_count = ebook_count
 
 # Calls the class
 my_new_ereader = Ereader("Amazon Kindle", "Paperwhite", "Adjustable Backlight" , "Several months of battery life" , "700 DPI")
 print(my_new_ereader.get_ereader_name())
 
-# Read library count
+my_new_ereader.update_library_count(48)
 my_new_ereader.read_library_count()
+
