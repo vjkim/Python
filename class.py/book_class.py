ac7b8b5638bc5811620bcd1116a2a9eb788bc7bc
V Kim <vjk.nyc@gmail.com>
2020-08-24 20:42:16 -0400

    creating first class

--- class.py/book_class.py
+++ class.py/book_class.py
@@ -1,5 +1,39 @@
-### 8/20/2020
+### 8/20/2020 (edited 8/24/2020)
 ### Creating our first class
 
 # __init__ helps python's method to distiguish our names/python's name
+
+# define class ==> capitalized names are classes. 
+# it is empty because we need to put stuff in it. 
+
+
+class Book():
+	"""A class to create a book"""
+
+	# A function within a class is called a method. 
+	# __init__ is a method.
+	# Creates an instance.
+
+	def __init__(self, name, price, publisher):
+		"""Initialize the name, price, and publisher attributes"""
+
+		# passes self which means itself. only defines name, price, and publisher
+		self.name = name
+		self.price = price
+		self.publisher = publisher
+		# any of these variables are available with the instances below.
+		# called attributes
+
+	def hardback(self):
+		""" Simulate a hardback book."""
+		print(self.name.title() + " is a hardback book.")
+		# only one parameter 
+	def softback(self):
+		""" Simulate a book being a softback book."""
+		print(self.name.title() + " is a softback book.")
+
+	def ebook(self):
+		"""Simulate an ebook."""
+		print(self.name.title() + " is an ebook.")
+
 
