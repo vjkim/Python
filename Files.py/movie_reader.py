6e66d707f30edb1e6bc29ae54e60e5865d2aca42
V Kim <vjk.nyc@gmail.com>
2020-08-24 21:31:00 -0400

    deleted

--- Files.py/movie_reader.py
+++
@@ -1,8 +0,0 @@
-### 8/22/2020
-### Read an entire file
-
-file_path = 'Files.py/txt.py/copy_of_movies.txt'
-with open(file_path) as file_objects:
-	contents = file_objects.read()
-	print(contents.strip())
-
