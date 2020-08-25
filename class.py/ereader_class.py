8ff4d5514fdc996ea6739e73289534776d8bb17e
V Kim <vjk.nyc@gmail.com>
2020-08-24 20:48:07 -0400

    idk

--- class.py/ereader_class.py
+++ class.py/ereader_class.py
@@ -17,8 +17,6 @@ class Ereader():
  		long_name = self.make + " - " + self.model + ' - ' + self.backlight + ' - ' + self.battery + ' - ' + self.screen_type
  		return long_name.title()
 
- 
 
-
-my_new_ereader = Ereader('Amazon Kindle', 'Paperwhite', 'Adjustable Backlight' , 'Several months of battery life' , '700 DPI')
- print(my_new_ereader.get_ereader_name())
+my_new_ereader = Ereader("Amazon Kindle", "Paperwhite", "Adjustable Backlight" , "Several months of battery life" , "700 DPI")
+print(my_new_ereader.get_ereader_name())
